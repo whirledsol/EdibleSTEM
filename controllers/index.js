c296506943a923f93a522d431e7142c943e6119c
unknown <wrhodes@CAS-O760-WR.CAS-PA01.viiad.com>
2015-10-16 14:06:06 -0400

    Created helper class in home page controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+/** HELPERS */
+
+var vars = new Object;
+	
+	/**
+		From Parameters, creates a block on the page for the carousel
+	*/
+	vars.CreateBlock = function(imageurl,title,summary,url){
+		return '<p>Things!</p>';
+		};
+		
+	/**
+		Title
+	*/
+	vars.title = "Edible STEM";
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Edible STEM' });
+  res.render('index', vars);
 });
 
 module.exports = router;
