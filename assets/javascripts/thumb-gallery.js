7f523f76262f04b4a04affc27905fccaf090dc5f
Will <astro491@gmail.com>
2015-12-01 20:30:51 -0500

    Lightbox

--- assets/javascripts/thumb-gallery.js
+++ assets/javascripts/thumb-gallery.js
@@ -12,25 +12,18 @@ $(window).on("load", function(){
 
 $(document).ready(function(){
 	
-		$(".thumb-gallery img").hover(function(){
-			var parent = $(this).parent();
-			var title = $(this).data("title");
-			if(title !== undefined){
+		$(".thumb-gallery li").hover(function(){
+
+				var title = $(this).data("title") !== undefined ? $(this).data("title") : "";
 				var titleElement = $('<div class="thumb-gallery-title">'+title+'</div>');
-				parent.append(titleElement);
-				titleElement.css("top",(parent.height() - titleElement.height())/2.0);
-				titleElement.css("left",(parent.width() - titleElement.width())/2.0);
-				titleElement.css("max-width",parent.width());
-				
-				
-				titleElement.fadeIn(700);
-			}
+				$(this).append(titleElement);
+				titleElement.css("top",($(this).height() - titleElement.height())/2.0);
+				titleElement.css("left",($(this).width() - titleElement.width())/2.0);
+				titleElement.css("max-width",$(this).width());
+			titleElement.fadeIn(700);
+			
 		},function(){
-			console.dir($(".thumb-gallery-title:hover"));
-			if($(".thumb-gallery-title:hover").length == 0){
-				var parent = $(this).parent();
-				parent.children(".thumb-gallery-title").remove();
-			}
+			$(this).children(".thumb-gallery-title").remove();
 		});
 	
 });
